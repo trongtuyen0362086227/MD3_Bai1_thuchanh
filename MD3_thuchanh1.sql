9baa000a1341ac0ae17d3757ff6f6caea9109d4a
unknown <you@example.com>
2022-11-07 10:33:06 -0800

    commit1

---
+++ MD3_thuchanh1.sql
@@ -0,0 +1,9 @@
+create Database Bai_1_LMS;
+use bai_1_lms;
+drop database Bai_1_LMS;
+create table test_management (
+id int not null primary key,
+name_use varchar(45) null,
+age int null,
+country varchar(45) null
+)
